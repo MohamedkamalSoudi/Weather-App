891d994099a85e943dd06b2d413251d20cc07036
MohamedkamalSoudi <mk0875734@gmail.com>
2024-06-30 18:41:44 +0300

    Edit  Api

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,17 +1,28 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:dio/dio.dart';
 import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService {
   final Dio dio;
-
+  final String baseUrl = 'https://api.weatherapi.com/v1';
+  final String apikey = '5e6ed2285b65436a9f813120231408';
   WeatherService(this.dio);
   Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response = await dio.get(
-        'https://api.weatherapi.com/v1/forecast.json?key=5e6ed2285b65436a9f813120231408&q=$cityName&days=1');
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apikey&q=$cityName&days=1');
 
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errMessage = e.response?.data['error']['message'] ??
+          'oops there was an error, try later';
+      throw Exception(errMessage);
+    } catch (e) {
+      log(e.toString());
+      throw Exception('oops there was an error, try later');
+    }
   }
 }
--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -9,35 +14,32 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Search a City'
-        ),
+        title: const Text('Search a City'),
         backgroundColor: Colors.blue,
       ),
-      body:  Padding(
-        padding:EdgeInsets.symmetric(horizontal: 16),
-        child:  Center(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Center(
           child: TextField(
-            onSubmitted: (value){
-              
+            onSubmitted: (value) async {
+              WeatherModel weatherModel = await WeatherService(Dio())
+                  .getCurrentWeather(cityName: value);
+              Navigator.pop(context);
             },
             decoration: InputDecoration(
-              contentPadding: EdgeInsets.symmetric(
-                vertical: 32,
-                horizontal: 16
-              ),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 32, horizontal: 16),
               labelText: 'Search',
               suffixIcon: const Icon(Icons.search),
               hintText: 'Enter City Name',
               border: OutlineInputBorder(
                 borderSide: const BorderSide(
                   color: Colors.green,
-              
+                ),
               ),
             ),
           ),
         ),
-      ),
       ),
     );
   }
